5260752098a66d56d7ab726eea68fc081517525e
Corentin Le Molgat <corentinl@google.com>
2018-02-08 11:06:16 +0100

    Update CMake UT using docker

---
+++ cmake/docker/alpine/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+set -x
+apk add --no-cache git build-base linux-headers cmake zlib-dev
--- docker/ubuntu/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-apt-get update -qq
-apt-get install -qq git build-essential cmake
