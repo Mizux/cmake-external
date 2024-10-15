a14596201a302e79731efb35feec1c4f7be7bd07
Mizux Seiha <mizux.dev@gmail.com>
2024-10-15 21:43:04 +0200

    ci: rework Makefile and dockers

--- ci/samples/main.cpp
+++ ci/samples/main.cpp
@@ -18,6 +18,10 @@ int main(int argc, char* argv[]) {
     LOG(INFO) << "Joined string: " << s << "\n";
   }
 
+  foo::freeFunction(0);
+  bar::freeFunction(1);
+  std::cout << std::endl;
+
   foo::Foo::staticFunction(int{0});
   bar::Bar::staticFunction(int{1});
   std::cout << std::endl;
