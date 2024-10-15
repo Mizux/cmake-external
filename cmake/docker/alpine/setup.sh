b459e07d6083b9164c68bb59a35eb0a101fd023b
Mizux Seiha <mizux.dev@gmail.com>
2024-10-16 01:17:29 +0200

    cmake: remove dead code

--- cmake/docker/alpine/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-set -x
-apk add --no-cache git build-base linux-headers cmake zlib-dev
--- cmake/sample/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <foo/Foo.hpp>
-
-int main(int argc, char** argv) {
-	foo::helloWorld();
-	return 0;
-}
