0b2276fc557cabd4378036ebbed300b596f8dc5b
Mizux Seiha <mizux.dev@gmail.com>
2025-09-10 23:02:25 +0200

    rework tests

--- Bar/tests/bar_test.cpp
+++ Bar/tests/bar_test.cpp
@@ -1,154 +1,149 @@
+#include <bar/Bar.hpp>
+
+#include <gtest/gtest.h>
 #include <iostream>
 #include <numeric>
 #include <string>
 
-#include <bar/Bar.hpp>
-
 namespace bar {
 
-TEST_CASE("Bar free function", "[Bar]") {
-  SECTION("Int Function") { REQUIRE_NOTHROW(freeFunction(42)); }
-  SECTION("Int64_t Function") { REQUIRE_NOTHROW(freeFunction(int64_t{42})); }
+TEST(BarTest, FreeFunction) {
+  EXPECT_NO_THROW(freeFunction(42));
+  EXPECT_NO_THROW(freeFunction(int64_t{42}));
 }
 
-TEST_CASE("String Vector usage", "[Bar]") {
-  SECTION("Vector of String Output") {
-    std::vector<std::string> result;
-    REQUIRE_NOTHROW(result = stringVectorOutput(8));
-    REQUIRE(result.size() == 8);
-    for (const auto& it : result) {
-      REQUIRE(it == std::to_string(8));
-    }
-  }
-  SECTION("Vector of String Input by value") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorInput(data));
-    REQUIRE(size == 5);
-  }
-  SECTION("Vector of String Input by const ref") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorRefInput(data));
-    REQUIRE(size == 5);
+TEST(BarTest, StringVectorOutput) {
+  std::vector<std::string> result;
+  ASSERT_NO_THROW(result = stringVectorOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (const auto& it : result) {
+    EXPECT_EQ(it, std::to_string(8));
   }
 }
 
-TEST_CASE("String Jagged Array usage", "[Bar]") {
-  SECTION("Jagged Array of String Output") {
-    std::vector<std::vector<std::string>> result;
-    REQUIRE_NOTHROW(result = stringJaggedArrayOutput(8));
-    REQUIRE(result.size() == 8);
-    for (int i = 0; i < result.size(); ++i) {
-      REQUIRE(i + 1 == result[i].size());
-    }
-    for (int i = 1; i <= result.size(); ++i) {
-      const auto& inner = result[i - 1];
-      for (const auto& it : inner) {
-        REQUIRE(it == std::to_string(i));
-      }
-    }
-  }
-  SECTION("Jagged Array of String Input by value") {
-    std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
-    int                                   size = 0;
-    REQUIRE_NOTHROW(size = stringJaggedArrayInput(data));
-    REQUIRE(size == 2);
-  }
-  SECTION("Jagged Array of String Input by const ref") {
-    std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
-    int                                   size = 0;
-    REQUIRE_NOTHROW(size = stringJaggedArrayRefInput(data));
-    REQUIRE(size == 2);
-  }
+TEST(BarTest, StringVectorValueInput) {
+  const std::vector<std::string> data{"1", "2", "3", "4", "5"};
+  int                            size = 0;
+  ASSERT_NO_THROW(size = stringVectorInput(data));
+  EXPECT_EQ(size, 5);
 }
 
-TEST_CASE("Pair Vector usage", "[Bar]") {
-  SECTION("Vector of Pair Output") {
-    std::vector<std::string> result;
-    REQUIRE_NOTHROW(result = stringVectorOutput(8));
-    REQUIRE(result.size() == 8);
-    for (const auto& it : result) {
-      REQUIRE(it == std::to_string(8));
-    }
-  }
-  SECTION("Vector of Pair Input by value") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorInput(data));
-    REQUIRE(size == 5);
-  }
-  SECTION("Vector of Pair Input by const ref") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorRefInput(data));
-    REQUIRE(size == 5);
-  }
+TEST(BarTest, StringVectorRefInput) {
+  const std::vector<std::string> data{"1", "2", "3", "4", "5"};
+  int                            size = 0;
+  ASSERT_NO_THROW(size = stringVectorRefInput(data));
+  EXPECT_EQ(size, 5);
 }
 
-TEST_CASE("Pair Jagged Array usage", "[Bar]") {
-  SECTION("Jagged Array of Pair Output") {
-    std::vector<std::vector<std::pair<int, int>>> result;
-    REQUIRE_NOTHROW(result = pairJaggedArrayOutput(8));
-    REQUIRE(result.size() == 8);
-    for (int i = 0; i < result.size(); ++i) {
-      REQUIRE(i + 1 == result[i].size());
-    }
-    for (int i = 1; i <= result.size(); ++i) {
-      const auto& inner = result[i - 1];
-      for (const auto& it : inner) {
-        REQUIRE(it == std::make_pair(i, i));
-      }
-    }
+TEST(BarTest, StringJaggedArrayOutput) {
+  std::vector<std::vector<std::string>> result;
+  ASSERT_NO_THROW(result = stringJaggedArrayOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (std::size_t i = 0; i < result.size(); ++i) {
+    EXPECT_EQ(i + 1, result[i].size());
   }
-  SECTION("Jagged Array of Pair Input by value") {
-    std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
-    int                                           size = 0;
-    REQUIRE_NOTHROW(size = pairJaggedArrayInput(data));
-    REQUIRE(size == 2);
-  }
-  SECTION("Jagged Array of Pair Input by const ref") {
-    std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
-    int                                           size = 0;
-    REQUIRE_NOTHROW(size = pairJaggedArrayRefInput(data));
-    REQUIRE(size == 2);
-  }
-}
-
-TEST_CASE("Bar static method", "[Bar]") {
-  SECTION("Int Method") { REQUIRE_NOTHROW(Bar::staticFunction(42)); }
-  SECTION("Int64_t Method") { REQUIRE_NOTHROW(Bar::staticFunction(int64_t{42})); }
-}
-
-TEST_CASE("Bar::Ctor", "[Bar]") {
-  SECTION("Default constructor") {
-    Bar* b = new Bar();
-    REQUIRE(b != nullptr);
-  }
-}
-
-SCENARIO("Bar Int", "[Bar]") {
-  GIVEN("A Bar instance") {
-    Bar bar;
-    WHEN("Setting a value") {
-      REQUIRE_NOTHROW(bar.setInt(42));
-      THEN("The value is updated") { REQUIRE(bar.getInt() == 42); }
+  for (std::size_t i = 1; i <= result.size(); ++i) {
+    const auto& inner = result[i - 1];
+    for (const auto& it : inner) {
+      EXPECT_EQ(it, std::to_string(i));
     }
   }
 }
 
-SCENARIO("Bar Int64", "[Bar]") {
-  GIVEN("A Bar instance") {
-    Bar bar;
-    WHEN("Setting a value") {
-      REQUIRE_NOTHROW(bar.setInt64(31));
-      THEN("The value is updated") { REQUIRE(bar.getInt64() == 31); }
+TEST(BarTest, StringJaggedArrayValueInput) {
+  const std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
+  int                                         size = 0;
+  ASSERT_NO_THROW(size = stringJaggedArrayInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(BarTest, StringJaggedArrayRefInput) {
+  const std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
+  int                                         size = 0;
+  ASSERT_NO_THROW(size = stringJaggedArrayRefInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(BarTest, PairVectorOutput) {
+  std::vector<std::pair<int, int>> result;
+  ASSERT_NO_THROW(result = pairVectorOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (const auto& it : result) {
+    EXPECT_EQ(it.first, 8);
+    EXPECT_EQ(it.second, 8);
+  }
+}
+
+TEST(BarTest, PairVectorValueInput) {
+  const std::vector<std::pair<int, int>> data{{1, 2}, {3, 4}, {5, 6}};
+  int                                    size = 0;
+  ASSERT_NO_THROW(size = pairVectorInput(data));
+  EXPECT_EQ(size, 3);
+}
+
+TEST(BarTest, PairVectorRefInput) {
+  const std::vector<std::pair<int, int>> data{{1, 2}, {3, 4}, {5, 6}};
+  int                                    size = 0;
+  ASSERT_NO_THROW(size = pairVectorRefInput(data));
+  EXPECT_EQ(size, 3);
+}
+
+TEST(BarTest, PairJaggedArrayOutput) {
+  std::vector<std::vector<std::pair<int, int>>> result;
+  ASSERT_NO_THROW(result = pairJaggedArrayOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (std::size_t i = 0; i < result.size(); ++i) {
+    EXPECT_EQ(i + 1, result[i].size());
+  }
+  for (int i = 1; i <= static_cast<int>(result.size()); ++i) {
+    const auto& inner = result[i - 1];
+    for (const auto& it : inner) {
+      EXPECT_EQ(it, std::make_pair(i, i));
     }
   }
 }
 
-TEST_CASE("Bar::operator()", "[Bar]") {
-  SECTION("Debug print") { INFO("Bar: " << Bar()()); }
+TEST(BarTest, PairJaggedArrayValueInput) {
+  std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
+  int                                           size = 0;
+  ASSERT_NO_THROW(size = pairJaggedArrayInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(BarTest, PairJaggedArrayRefInput) {
+  std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
+  int                                           size = 0;
+  ASSERT_NO_THROW(size = pairJaggedArrayRefInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(BarTest, StaticMethods) {
+  EXPECT_NO_THROW(Bar::staticFunction(42));
+  EXPECT_NO_THROW(Bar::staticFunction(int64_t{42}));
+}
+
+TEST(BarTest, Constructor) {
+  Bar* b = new Bar();
+  EXPECT_NE(b, nullptr);
+}
+
+TEST(BarTest, IntMethods) {
+  Bar bar;
+  ASSERT_NO_THROW(bar.setInt(42));
+  EXPECT_EQ(42, bar.getInt());
+}
+
+TEST(BarTest, Int64Methods) {
+  Bar bar;
+  ASSERT_NO_THROW(bar.setInt64(31));
+  EXPECT_EQ(31, bar.getInt64());
+}
+
+TEST(BarTest, PrintMethod) {
+  Bar         bar;
+  std::string str("");
+  ASSERT_NO_THROW(str = bar());
+  EXPECT_EQ("\"Bar\":{\"int\":0,\"int64\":0}", str);
 }
 
 } // namespace bar
--- Foo/tests/foo_test.cpp
+++ Foo/tests/foo_test.cpp
@@ -1,154 +1,149 @@
+#include <foo/Foo.hpp>
+
+#include <gtest/gtest.h>
 #include <iostream>
 #include <numeric>
 #include <string>
 
-#include <foo/Foo.hpp>
-
 namespace foo {
 
-TEST_CASE("Foo free function", "[Foo]") {
-  SECTION("Int Function") { REQUIRE_NOTHROW(freeFunction(42)); }
-  SECTION("Int64_t Function") { REQUIRE_NOTHROW(freeFunction(int64_t{42})); }
+TEST(FooTest, FreeFunction) {
+  EXPECT_NO_THROW(freeFunction(42));
+  EXPECT_NO_THROW(freeFunction(int64_t{42}));
 }
 
-TEST_CASE("String Vector usage", "[Foo]") {
-  SECTION("Vector of String Output") {
-    std::vector<std::string> result;
-    REQUIRE_NOTHROW(result = stringVectorOutput(8));
-    REQUIRE(result.size() == 8);
-    for (const auto& it : result) {
-      REQUIRE(it == std::to_string(8));
-    }
-  }
-  SECTION("Vector of String Input by value") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorInput(data));
-    REQUIRE(size == 5);
-  }
-  SECTION("Vector of String Input by const ref") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorRefInput(data));
-    REQUIRE(size == 5);
+TEST(FooTest, StringVectorOutput) {
+  std::vector<std::string> result;
+  ASSERT_NO_THROW(result = stringVectorOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (const auto& it : result) {
+    EXPECT_EQ(it, std::to_string(8));
   }
 }
 
-TEST_CASE("String Jagged Array usage", "[Foo]") {
-  SECTION("Jagged Array of String Output") {
-    std::vector<std::vector<std::string>> result;
-    REQUIRE_NOTHROW(result = stringJaggedArrayOutput(8));
-    REQUIRE(result.size() == 8);
-    for (int i = 0; i < result.size(); ++i) {
-      REQUIRE(i + 1 == result[i].size());
-    }
-    for (int i = 1; i <= result.size(); ++i) {
-      const auto& inner = result[i - 1];
-      for (const auto& it : inner) {
-        REQUIRE(it == std::to_string(i));
-      }
-    }
-  }
-  SECTION("Jagged Array of String Input by value") {
-    std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
-    int                                   size = 0;
-    REQUIRE_NOTHROW(size = stringJaggedArrayInput(data));
-    REQUIRE(size == 2);
-  }
-  SECTION("Jagged Array of String Input by const ref") {
-    std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
-    int                                   size = 0;
-    REQUIRE_NOTHROW(size = stringJaggedArrayRefInput(data));
-    REQUIRE(size == 2);
-  }
+TEST(FooTest, StringVectorValueInput) {
+  const std::vector<std::string> data{"1", "2", "3", "4", "5"};
+  int                            size = 0;
+  ASSERT_NO_THROW(size = stringVectorInput(data));
+  EXPECT_EQ(size, 5);
 }
 
-TEST_CASE("Pair Vector usage", "[Foo]") {
-  SECTION("Vector of Pair Output") {
-    std::vector<std::string> result;
-    REQUIRE_NOTHROW(result = stringVectorOutput(8));
-    REQUIRE(result.size() == 8);
-    for (const auto& it : result) {
-      REQUIRE(it == std::to_string(8));
-    }
-  }
-  SECTION("Vector of Pair Input by value") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorInput(data));
-    REQUIRE(size == 5);
-  }
-  SECTION("Vector of Pair Input by const ref") {
-    std::vector<std::string> data{"1", "2", "3", "4", "5"};
-    int                      size = 0;
-    REQUIRE_NOTHROW(size = stringVectorRefInput(data));
-    REQUIRE(size == 5);
-  }
+TEST(FooTest, StringVectorRefInput) {
+  const std::vector<std::string> data{"1", "2", "3", "4", "5"};
+  int                            size = 0;
+  ASSERT_NO_THROW(size = stringVectorRefInput(data));
+  EXPECT_EQ(size, 5);
 }
 
-TEST_CASE("Pair Jagged Array usage", "[Foo]") {
-  SECTION("Jagged Array of Pair Output") {
-    std::vector<std::vector<std::pair<int, int>>> result;
-    REQUIRE_NOTHROW(result = pairJaggedArrayOutput(8));
-    REQUIRE(result.size() == 8);
-    for (int i = 0; i < result.size(); ++i) {
-      REQUIRE(i + 1 == result[i].size());
-    }
-    for (int i = 1; i <= result.size(); ++i) {
-      const auto& inner = result[i - 1];
-      for (const auto& it : inner) {
-        REQUIRE(it == std::make_pair(i, i));
-      }
-    }
+TEST(FooTest, StringJaggedArrayOutput) {
+  std::vector<std::vector<std::string>> result;
+  ASSERT_NO_THROW(result = stringJaggedArrayOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (std::size_t i = 0; i < result.size(); ++i) {
+    EXPECT_EQ(i + 1, result[i].size());
   }
-  SECTION("Jagged Array of Pair Input by value") {
-    std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
-    int                                           size = 0;
-    REQUIRE_NOTHROW(size = pairJaggedArrayInput(data));
-    REQUIRE(size == 2);
-  }
-  SECTION("Jagged Array of Pair Input by const ref") {
-    std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
-    int                                           size = 0;
-    REQUIRE_NOTHROW(size = pairJaggedArrayRefInput(data));
-    REQUIRE(size == 2);
-  }
-}
-
-TEST_CASE("Foo static method", "[Foo]") {
-  SECTION("Int Method") { REQUIRE_NOTHROW(Foo::staticFunction(42)); }
-  SECTION("Int64_t Method") { REQUIRE_NOTHROW(Foo::staticFunction(int64_t{42})); }
-}
-
-TEST_CASE("Foo::Ctor", "[Foo]") {
-  SECTION("Default constructor") {
-    Foo* b = new Foo();
-    REQUIRE(b != nullptr);
-  }
-}
-
-SCENARIO("Foo Int", "[Foo]") {
-  GIVEN("A Foo instance") {
-    Foo foo;
-    WHEN("Setting a value") {
-      REQUIRE_NOTHROW(foo.setInt(42));
-      THEN("The value is updated") { REQUIRE(foo.getInt() == 42); }
+  for (std::size_t i = 1; i <= result.size(); ++i) {
+    const auto& inner = result[i - 1];
+    for (const auto& it : inner) {
+      EXPECT_EQ(it, std::to_string(i));
     }
   }
 }
 
-SCENARIO("Foo Int64", "[Foo]") {
-  GIVEN("A Foo instance") {
-    Foo foo;
-    WHEN("Setting a value") {
-      REQUIRE_NOTHROW(foo.setInt64(31));
-      THEN("The value is updated") { REQUIRE(foo.getInt64() == 31); }
+TEST(FooTest, StringJaggedArrayValueInput) {
+  const std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
+  int                                         size = 0;
+  ASSERT_NO_THROW(size = stringJaggedArrayInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(FooTest, StringJaggedArrayRefInput) {
+  const std::vector<std::vector<std::string>> data{{"1", "2", "3"}, {"4", "5"}};
+  int                                         size = 0;
+  ASSERT_NO_THROW(size = stringJaggedArrayRefInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(FooTest, PairVectorOutput) {
+  std::vector<std::pair<int, int>> result;
+  ASSERT_NO_THROW(result = pairVectorOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (const auto& it : result) {
+    EXPECT_EQ(it.first, 8);
+    EXPECT_EQ(it.second, 8);
+  }
+}
+
+TEST(FooTest, PairVectorValueInput) {
+  const std::vector<std::pair<int, int>> data{{1, 2}, {3, 4}, {5, 6}};
+  int                                    size = 0;
+  ASSERT_NO_THROW(size = pairVectorInput(data));
+  EXPECT_EQ(size, 3);
+}
+
+TEST(FooTest, PairVectorRefInput) {
+  const std::vector<std::pair<int, int>> data{{1, 2}, {3, 4}, {5, 6}};
+  int                                    size = 0;
+  ASSERT_NO_THROW(size = pairVectorRefInput(data));
+  EXPECT_EQ(size, 3);
+}
+
+TEST(FooTest, PairJaggedArrayOutput) {
+  std::vector<std::vector<std::pair<int, int>>> result;
+  ASSERT_NO_THROW(result = pairJaggedArrayOutput(8));
+  EXPECT_EQ(result.size(), 8);
+  for (std::size_t i = 0; i < result.size(); ++i) {
+    EXPECT_EQ(i + 1, result[i].size());
+  }
+  for (int i = 1; i <= static_cast<int>(result.size()); ++i) {
+    const auto& inner = result[i - 1];
+    for (const auto& it : inner) {
+      EXPECT_EQ(it, std::make_pair(i, i));
     }
   }
 }
 
-TEST_CASE("Foo::operator()", "[Foo]") {
-  SECTION("Debug print") { INFO("Foo: " << Foo()()); }
+TEST(FooTest, PairJaggedArrayValueInput) {
+  std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
+  int                                           size = 0;
+  ASSERT_NO_THROW(size = pairJaggedArrayInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(FooTest, PairJaggedArrayRefInput) {
+  std::vector<std::vector<std::pair<int, int>>> data{{{1, 1}, {2, 2}, {3, 3}}, {{4, 4}, {5, 5}}};
+  int                                           size = 0;
+  ASSERT_NO_THROW(size = pairJaggedArrayRefInput(data));
+  EXPECT_EQ(size, 2);
+}
+
+TEST(FooTest, StaticMethods) {
+  EXPECT_NO_THROW(Foo::staticFunction(42));
+  EXPECT_NO_THROW(Foo::staticFunction(int64_t{42}));
+}
+
+TEST(FooTest, Constructor) {
+  Foo* b = new Foo();
+  EXPECT_NE(b, nullptr);
+}
+
+TEST(FooTest, IntMethods) {
+  Foo foo;
+  ASSERT_NO_THROW(foo.setInt(42));
+  EXPECT_EQ(42, foo.getInt());
+}
+
+TEST(FooTest, Int64Methods) {
+  Foo foo;
+  ASSERT_NO_THROW(foo.setInt64(31));
+  EXPECT_EQ(31, foo.getInt64());
+}
+
+TEST(FooTest, PrintMethod) {
+  Foo         foo;
+  std::string str("");
+  ASSERT_NO_THROW(str = foo());
+  EXPECT_EQ("\"Foo\":{\"int\":0,\"int64\":0}", str);
 }
 
 } // namespace foo
